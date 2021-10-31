bc09434887b5a269ac31f319758049fc7417afb9
mdhimasqi <mdhimasqi@gmail.com>
2021-10-31 17:27:59 +0700

    Desc Product - See Product from the Picture

---
+++ cypress/integration/sauce5.specs.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('Description Product  - 1941720165 / Mochammad Dimasqi Aliffudin Faiz', () => {
+
+    beforeEach(() => {
+        cy.visit('/')
+        cy.get('[data-test=username]').type('standard_user')
+        cy.get('[data-test=password]').type('secret_sauce')
+        cy.get('[data-test=login-button]').click()
+    })
+    
+    it('See Product from the Picture ', () => {
+        cy.get('#item_4_img_link').click()
+        cy.contains('Sauce Labs Backpack').should('be.visible')
+    })
+
+    
+})
